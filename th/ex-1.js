c13ccc5761aae022a84d1af84995c6df8179f145
parinTNK <parin.tnk@gmail.com>
2025-04-04 11:38:25 +0700

    submit

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,9 +1,47 @@
 const moveZeroes = function (nums) {
-  //Start coding here
-};
+
+let arr = []
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      arr.push(nums[i]);
+    }
+  }
+  console.log(arr);
+  for (let i = arr.length; i < nums.length; i++) {
+    arr.push(0);
+  }
+  console.log(arr);
+  return arr;
+}
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
 console.log(result1); // [1,3,12,0,0]
 
 const result2 = moveZeroes([0]);
 console.log(result2); // [0]
+
+
+// *####################################################################
+// *####################################################################
+// *####################################################################
+
+
+let num = [1,2,3,4,5,6,7,8,9]
+
+
+const evenAndOdd = (num) => {
+  let even = []
+  let odd = []
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] % 2 === 0) {
+      even.push(num[i])
+    } else {
+      odd.push(num[i])
+    }
+  }
+  return [...even, ...odd]
+}
+const result3 = evenAndOdd(num)
+console.log(result3);
+
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,29 @@
 const romanToInt = function (s) {
   //Start coding here
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let total = 0;
+  let prevValue = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    const currentValue = romanNumerals[s[i]];
+      // IV = 5 - 1 = 4
+      // VI = 5 + 1 = 6
+    if (currentValue < prevValue) {
+      total -= currentValue;
+    } else {
+      
+      total += currentValue;
+    }
+    prevValue = currentValue;
+  }
+  return total;
 };
 
 const result1 = romanToInt("III"); // 3
